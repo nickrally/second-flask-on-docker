a1cd54e817ab9b7f890367a7de404babdf43d2ce
nickrally <nickrally2009@gmail.com>
2022-10-16 18:56:53 -0600

    added postgres db service

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,6 +1,6 @@
 from flask.cli import FlaskGroup
 
-from project import app
+from project import app, db, Note
 
 
 cli = FlaskGroup(app)
@@ -9,6 +9,20 @@ cli = FlaskGroup(app)
 @cli.command("say_hi")
 def say_hi():
     print("HEY")
+
+
+@cli.command("create_db")
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@cli.command("seed_db")
+def seed_db():
+    db.session.add(Note(english="A man who wishes to seem simple cannot possibly be a simpleton.",
+                   spanish="El hombre que desea parecer simple no puede ser un simplón."))
+    db.session.commit()
 
 
 if __name__ == "__main__":
--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config.from_object("project.config.Config")
+db = SQLAlchemy(app)
+
+
+class Note(db.Model):
+    __tablename__ = "note"
+
+    id = db.Column(db.Integer, primary_key=True)
+    english = db.Column(db.Text)
+    spanish = db.Column(db.Text)
+
+    def __init__(self, english, spanish):
+        self.english = english
+        self.spanish = spanish
 
 
 @app.route("/")
